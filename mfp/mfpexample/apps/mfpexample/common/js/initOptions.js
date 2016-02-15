1c397411dd3e114ed6c73e5941d429c762758763
vortex7 <sean.wright.vortex@gmail.com>
2016-02-15 14:22:45 -0500

    Mods

--- mfp/mfpexample/apps/mfpexample/common/js/initOptions.js
+++ mfp/mfpexample/apps/mfpexample/common/js/initOptions.js
@@ -1,29 +1,48 @@
 // Uncomment the initialization options as required. For advanced initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center 
  
  var wlInitOptions = {
-	
-	// # To disable automatic hiding of the splash screen uncomment this property and use WL.App.hideSplashScreen() API
-	//autoHideSplash: false,
-		 
-	// # The callback function to invoke in case application fails to connect to MobileFirst Server
-	//onConnectionFailure: function (){},
-	
-	// # MobileFirst Server connection timeout
-	//timeout: 30000,
-	
-	// # How often heartbeat request will be sent to MobileFirst Server
-	//heartBeatIntervalInSecs: 7 * 60,
-	
-	// # Enable FIPS 140-2 for data-in-motion (network) and data-at-rest (JSONStore) on iOS or Android.
-	//   Requires the FIPS 140-2 optional feature to be enabled also.
-	//enableFIPS : false,
-	
-	// # The options of busy indicator used during application start up
-	//busyOptions: {text: "Loading..."}
+    
+    // # To disable automatic hiding of the splash screen uncomment this property and use WL.App.hideSplashScreen() API
+    //autoHideSplash: false,
+         
+    // # The callback function to invoke in case application fails to connect to MobileFirst Server
+    //onConnectionFailure: function (){},
+    
+    // # MobileFirst Server connection timeout
+    //timeout: 30000,
+    
+    // # How often heartbeat request will be sent to MobileFirst Server
+    //heartBeatIntervalInSecs: 7 * 60,
+    
+    // # Enable FIPS 140-2 for data-in-motion (network) and data-at-rest (JSONStore) on iOS or Android.
+    //   Requires the FIPS 140-2 optional feature to be enabled also.
+    //enableFIPS : false,
+    
+    // # The options of busy indicator used during application start up
+    //busyOptions: {text: "Loading..."}
 };
 
 if (window.addEventListener) {
-	window.addEventListener('load', function() { WL.Client.init(wlInitOptions); }, false);
+    window.addEventListener('load', function() { WL.Client.init(wlInitOptions); }, false);
 } else if (window.attachEvent) {
-	window.attachEvent('onload',  function() { WL.Client.init(wlInitOptions); });
+    window.attachEvent('onload',  function() { WL.Client.init(wlInitOptions); });
+}
+
+var realm = "HeaderAuthRealm";
+var challengeHandler = WL.Client.createChallengeHandler(realm);
+
+challengeHandler.isCustomResponse = isCustomResponse;
+challengeHandler.handleChallenge = handleChallenge;
+challengeHandler.handleFailure = handleFailure;
+
+function isCustomResponse(response) {
+    console.log("*************WL.Client:isCustomResponse***************");
+}
+
+function handleChallenge(response) {
+    console.log("*************WL.Client:handleChallenge***************");
+}
+
+function handleFailure(response) {
+    console.log("*************WL.Client:handleFailure***************");
 }
--- mfp/mfpexample/apps/mfpexample/common/js/main.js
+++ mfp/mfpexample/apps/mfpexample/common/js/main.js
@@ -1,16 +1,43 @@
 function wlCommonInit(){
-	/*
-	 * Use of WL.Client.connect() API before any connectivity to a MobileFirst Server is required. 
-	 * This API should be called only once, before any other WL.Client methods that communicate with the MobileFirst Server.
-	 * Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
-	
-	// Common initialization code goes here
-	
+    /*
+     * Use of WL.Client.connect() API before any connectivity to a MobileFirst Server is required. 
+     * This API should be called only once, before any other WL.Client methods that communicate with the MobileFirst Server.
+     * Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
+     *    
+     *    WL.Client.connect({
+     *            onSuccess: onConnectSuccess,
+     *            onFailure: onConnectFailure
+     *    });
+     *     
+     */
+    
+    // Common initialization code goes here
+    /*
+    WL.Client.connect({
+        onSuccess :function(){ console.log("****** WL.Client.connect: success ********")},
+        onFailure :function(){ console.log("****** WL.Client.connect: fail ********");},
+        timeout :5000
+    });
+    */
+    
+}
+
+function wlClientConnect() {
+    WL.Client.connect({
+        onSuccess :function(){ console.log("****** WL.Client.connect: success ********")},
+        onFailure :function(){ console.log("****** WL.Client.connect: fail ********");},
+        timeout :5000
+    });
+}
+
+function login() {
+    $.ajax({url: "http://192.168.43.211:5000/login", success: function(result){
+        alert("Login success");
+    }});
+}
+
+function logout() {
+    $.ajax({url: "http://192.168.43.211:5000/logout", success: function(result){
+        alert("Logout success");
+    }});
 }
